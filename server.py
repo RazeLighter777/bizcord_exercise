dd327e548f0944a7dfa20d7636efffd926ead132
Justin Suess <justin.suess@westpoint.edu>
2024-03-07 17:30:37 -0500

    added some stuff

--- server.py
+++ server.py
@@ -6,13 +6,29 @@ import json
 app = Flask(__name__)
 sock = Sock(app)
 
+app.secret_key = "11234132446"
+
 @app.route('/')
 def index():
 	return render_template('index.html')
+	
 
 @app.route('/static/<path:path>')
 def send_static(path):
 	return send_from_directory('static', path)
 
+@app.route('/login', methods = ['POST'])
+def login():
+    data = request.form
+    session["user"] = data["username"]
+    return {}
+    
+@app.route("/get_session", methods = ['GET'])
+def get_session():
+    if session["user"]:
+        return {"user" : session["user"]}
+    return {}
+    
+    
 if __name__ == "__main__":
 	app.run(debug=True)
