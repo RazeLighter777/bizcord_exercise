1b4566126b7e81fc3adfa1e4acd6d92dbe7876ae
Justin Suess <justin.suess@westpoint.edu>
2024-03-07 17:58:18 -0500

    asdf

--- server.py
+++ server.py
@@ -19,15 +19,30 @@ def send_static(path):
 
 @app.route('/login', methods = ['POST'])
 def login():
-    data = request.form
-    session["user"] = data["username"]
-    return {}
+    session["user"] = request.form["username"]    
+    user = None
+    if not getUser(request.form["username"]):
+        user = createUser(request.form["username"])
+    else:
+        user = getUser(request.form["username"])
+    resp = make_response('{"loggedIn" : true}')
+    resp.set_cookie("session_token", user['session_token'])
+    return resp
+    
+@app.route('/rooms', methods = ['GET'])
+def get_rooms():
+    print(getRooms())
+    return {"rooms" : getRooms() }
+
+@app.route('/room/<roomname>/', methods = ['GET'])
+def get_room(roomname):
+    return {"room" : getRoom(roomname)}
     
 @app.route("/get_session", methods = ['GET'])
 def get_session():
-    if session["user"]:
-        return {"user" : session["user"]}
-    return {}
+    if request.cookies["session_token"]:
+        return {"user" : getUserBySessionToken(request.cookies["session_token"])}
+	
     
     
 if __name__ == "__main__":
